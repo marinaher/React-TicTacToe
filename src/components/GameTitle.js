7ee6fd105b7e22160807df6d7bc26e4d2f631d53
Marina Her <Marina@Marinas-MacBook-Pro.local>
2017-03-27 17:42:31 -0500

    changed file name from Header.js - Not a Header

---
+++ src/components/GameTitle.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+export default class GameTitle extends Component {
+  render() {
+    return(
+      <div className="game-title">
+        <h1>
+          Tic <br/> Tac <br/> Toe
+        </h1>
+      </div>
+    );
+  }
+}
