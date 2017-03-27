5fd1fd4150bbad8ca34ac992f70ba2b11ed975dd
Marina Her <Marina@Marinas-MacBook-Pro.local>
2017-03-27 18:26:02 -0500

    Updated line break in title

--- src/containers/App.js
+++ src/containers/App.js
@@ -5,11 +5,13 @@ import Board from '../components/Board';
 
 class App extends Component {
   render() {
+    const title = 'Tic \nTac \nToe';
+
     return (
       <div className="container">
         <div className="gameMenu">
           <div className="game-title">
-            <h1>Tic <br/> Tac <br/> Toe</h1>
+            <h1 className="display-linebreak">{title}</h1>
           </div>
 
           <Board />
