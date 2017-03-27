f38c9542bf3a64e76b94ddd1dca3a9b7ed24a255
Marina Her <Marina@Marinas-MacBook-Pro.local>
2017-03-27 17:45:56 -0500

    Moved ResetButton component from App.js to Board.js

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,19 +1,16 @@
 import React, { Component } from 'react';
 import '../styles/App.css';
 
-// import Game from '../components/Game';
 import Board from '../components/Board';
-import Header from '../components/Header';
-import ResetButton from '../components/ButtonComponents/ResetButton';
+import GameTitle from '../components/GameTitle';
 
 class App extends Component {
   render() {
     return (
       <div className="container">
         <div className="gameMenu">
-          <Header />
+          <GameTitle />
           <Board />
-          <ResetButton />
         </div>
       </div>
     );
