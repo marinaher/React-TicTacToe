70134a41ce73574c364eb5e9a839d3e086db76f5
Marina Her <Marina@Marinas-MacBook-Pro.local>
2017-03-27 15:46:42 -0500

    Added button components folder to add button files

---
+++ src/components/ButtonComponents/ResetButton.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../../styles/index.css';
+
+class ResetButton extends React.Component{
+  render(){
+    return(
+      <button>Reset Game</button>
+    );
+  }
+}
+
+export default ResetButton;
